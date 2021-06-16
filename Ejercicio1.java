bfd9b34f9be40ffb909bf3f5962badfe5ad23379
Christian Lugo <christianrlugo5@gmail.com>
2021-06-16 01:02:23 -0600

    Create Ejercicio1

---
+++ Ejercicio1.java
@@ -0,0 +1,12 @@
+public class Ejercicio1 {
+	
+	/*
+	 * ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+	 * If the function is passed a valid PIN string, return true, else return false.
+	 * */
+	
+	public static boolean validatePinCode(String pin) {		
+		return pin.matches("[0-9]{4}|[0-9]{6}");
+	}
+	
+}
